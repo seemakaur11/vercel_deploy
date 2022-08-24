2ee80ad7a699d280a2ee300465fcc2906bb2d0bd
seemakaur11 <raiseema248@gmail.com>
2022-08-24 10:55:03 -0400

    path joined

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => res.send('hello seema'));
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = 5005;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
